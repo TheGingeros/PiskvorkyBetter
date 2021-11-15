34af32f4cd9ee252d0608c40edcbb0c536398f46
TheGingeros <frankie.glazier01@gmail.com>
2021-11-15 18:16:16 +0100

    Update ale nefunguje ten win

--- main.py
+++ main.py
@@ -4,18 +4,97 @@ play = ""
 play1 = ""
 game_start = 0
 kreten= False
+game_loop = True
 
 
-for i in range(velikost):
-    plan.append([])
-    for j in range(velikost):
-        plan[i].append("_")
+#Vytváří pole a dává do listu prázdné pole
+def create_plan():
+    for i in range(velikost):
+        plan.append([])
+        for j in range(velikost):
+            plan[i].append("_")
 
-while play != "End":
+#Zobrazuje aktuální pole
+def show_plan():
+    for i in plan:
+        print(i)
+
+def assign_kolečko():
+    plan[play][play1] = "O"
+
+def assign_křížek():
+    plan[play][play1] = "X"
+def win_kolečko():
+    count = 0
+    general_count = 0
+    line_count = 0
+    kolečko_count = 0
+    pos_1 = 0
+    pos_2 = 0
+    pos_3 = 0
+    for line in plan:
+        general_count+=1
+        line_count += 1
+        for symbol in line:
+            count+=1
+            if symbol == "O":
+                kolečko_count += 1
+            else:
+                pass
+            if general_count == kolečko_count:
+                pos_1 += 1
+            if general_count + 1 == kolečko_count:
+                pos_2 += 1
+            if general_count + 2 == kolečko_count:
+                pos_3 += 1
+            else:
+                pass
+    if pos_1 == 1 and pos_2 == 1 and pos_3 == 1:
+        print("Kolečko vyhrálo!")
+        game_loop = False
+    else:
+        general_count = 0
+        count = 0
+        line_count = 0
+        kolečko_count = 0
+        pos_1 = 0
+        pos_2 = 0
+        pos_3 = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+create_plan()
+
+
+while game_loop == True:
     if game_start == 0:
         print("Tady máš pole: ")
-        for i in plan:
-            print(i)
+        show_plan()
     print("Hraješ jako kolečko: ")
     play = int(input("Jaký řádek? "))
     play1 = int(input("Jaká pozice? "))
@@ -25,8 +104,7 @@ while play != "End":
         kreten = True
         while kreten == True:
             print("Si kretén tam už něco je, zkus to znovu!")
-            for i in plan:
-                print(i)
+            show_plan()
             play = int(input("Jaký řádek? "))
             play1 = int(input("Jaká pozice? "))
             play -= 1
@@ -35,14 +113,15 @@ while play != "End":
                 kreten = True
             else:
                 kreten = False
-                plan[play][play1] = "O"
-                for i in plan:
-                    print(i)
+                assign_kolečko()
+                show_plan()
+                win_kolečko()
+
 
     else:
-        plan[play][play1]= "O"
-        for i in plan:
-            print(i)
+        assign_kolečko()
+        show_plan()
+        win_kolečko()
         game_start += 1
 
     print("Hraješ jako křížek: ")
@@ -54,8 +133,7 @@ while play != "End":
         kreten = True
         while kreten == True:
             print("Si kretén tam už něco je, zkus to znovu!")
-            for i in plan:
-                print(i)
+            show_plan()
             play = int(input("Jaký řádek? "))
             play1 = int(input("Jaká pozice? "))
             play -= 1
@@ -64,10 +142,8 @@ while play != "End":
                 kreten = True
             else:
                 kreten = False
-                plan[play][play1] = "X"
-                for i in plan:
-                    print(i)
+                assign_křížek()
+                show_plan()
     else:
-        plan[play][play1] = "X"
-        for i in plan:
-            print(i)
+        assign_křížek()
+        show_plan()
